45d16318f9d83be1271db400588c246d1aad28f5
Abhinav Ak <abhinavkukku48@gmail.com>
2024-01-11 22:06:13 +0530

    apps created and completed all template layout

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+
+    path('', views.index,name='home'),
+    path('product_list/',views.products_list,name='listof_products'),
+    path('product_detail/',views.products_details,name='detail_of_product'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def products_list(request):
+    return render(request,'products.html')
+
+def products_details(request):
+    return render(request,'product_detail.html')
